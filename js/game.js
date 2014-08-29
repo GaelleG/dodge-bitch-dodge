08ae06c067cc4c560fe3ce800a2c9c9d6fe493f6
Gaelle <pussy@pukogames.com>
2014-08-29 17:55:30 +0200

    AbstractViewport JS class

--- js/game.js
+++ js/game.js
@@ -1,3 +1,11 @@
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//
+//                               Client game loop
+//
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 // =============================================================================
 //                                  COMMON VARS
 // =============================================================================
@@ -9,51 +17,23 @@ var CANVAS_WIDTH = Math.min(
   document.documentElement.clientHeight, 
   window.innerHeight);
 var CANVAS_HEIGHT = CANVAS_WIDTH;
-var BOX_SIZE = CANVAS_HEIGHT/24;
-var BOX_NB_X = Math.floor(CANVAS_WIDTH/BOX_SIZE);
-var BOX_NB_Y = Math.floor(CANVAS_HEIGHT/BOX_SIZE);
-var ENEMY_MAX_SIZE = 3;
-var VIEWPORT_BOUNDS = {
-  left: 0,
-  top: 0,
-  right: CANVAS_WIDTH,
-  bottom: CANVAS_HEIGHT,
-};
-var ENEMY_VIEWPORT_BOUNDS = {
-  left: VIEWPORT_BOUNDS.left - ENEMY_MAX_SIZE*BOX_SIZE,
-  top: VIEWPORT_BOUNDS.top - ENEMY_MAX_SIZE*BOX_SIZE,
-  right: VIEWPORT_BOUNDS.right + ENEMY_MAX_SIZE*BOX_SIZE,
-  bottom: VIEWPORT_BOUNDS.bottom + ENEMY_MAX_SIZE*BOX_SIZE,
-};
-var DIRECTION = {
-  left:
-    { x:-1, y:0 },
-  right:
-    { x:1, y:0 },
-  top:
-    { x:0, y:-1 },
-  bottom:
-    { x:0, y:1 },
-};
+var BOX_SIZE = CANVAS_HEIGHT/AbstractViewport.BOX_NB_X;
+var BOX_NB_X = AbstractViewport.BOX_NB_X;
+var BOX_NB_Y = AbstractViewport.BOX_NB_Y;
+var ENEMY_MAX_SIZE = AbstractViewport.ENEMY_MAX_SIZE;
 
 // ---------------------------------------------------------------------- GLOBAL
 var enemies = [];
-var enemyDirection = null;
-var enemyMove = {
-  x: 0.0,
-  y: 0.0
-};
 var player = [];
-var playerDirection = { x:0, y:0 };
 var canvas;
 var menu;
 var play;
 
-function loadGame() {
+// =============================================================================
+//                                 GAME LOADING
+// =============================================================================
 
-// =============================================================================
-//                                   FUNCTIONS
-// =============================================================================
+function loadGame() {
 
 // ------------------------------------------------------------------------- DOM
 canvas = document.getElementById("glcanvas");
@@ -62,89 +42,60 @@ canvas.height = CANVAS_HEIGHT;
 menu = document.getElementById("menu");
 play = document.getElementById("play");
 
+// =============================================================================
+//                                   FUNCTIONS
+// =============================================================================
+
 // ---------------------------------------------------------------------- PLAYER
 function setPlayer() {
-  player = [
-    CANVAS_WIDTH/2 - BOX_SIZE*0.5/2, CANVAS_HEIGHT/2 - BOX_SIZE*0.5/2, 0.0,
-    CANVAS_WIDTH/2 + BOX_SIZE*0.5/2, CANVAS_HEIGHT/2 - BOX_SIZE*0.5/2, 0.0,
-    CANVAS_WIDTH/2 - BOX_SIZE*0.5/2, CANVAS_HEIGHT/2 + BOX_SIZE*0.5/2, 0.0,
-    CANVAS_WIDTH/2 + BOX_SIZE*0.5/2, CANVAS_HEIGHT/2 + BOX_SIZE*0.5/2, 0.0,
-  ];
+  AbstractViewport.setPlayer();
+  player = multiplyMatrix(AbstractViewport.player, BOX_SIZE);
+}
+
+function emptyPlayer() {
+  AbstractViewport.emptyPlayer();
+  player = [];
 }
 
 function movePlayer(delta) {
-  if (playerDirection.x == 0 && playerDirection.y == 0) {
-    return;
+  if (AbstractViewport.movePlayer(delta)) {
+    player = multiplyMatrix(AbstractViewport.player, BOX_SIZE);
   }
-  var move = {
-    x: playerDirection.x * 0.1*delta,
-    y: playerDirection.y * 0.1*delta,
-  };
-  var moveVertice = [
-    move.x, move.y, 0,
-    move.x, move.y, 0,
-    move.x, move.y, 0,
-    move.x, move.y, 0,
-  ];
-  player = addMatrix(player, moveVertice, VIEWPORT_BOUNDS);
-
 }
 
 // --------------------------------------------------------------------- ENEMIES
-var lastEnemySet = 0;
-var enemyInterval = 10000/((BOX_NB_X > BOX_NB_Y) ? BOX_NB_X : BOX_NB_Y);
 function setEnemy(delta) {
-  lastEnemySet += delta;
-  if (lastEnemySet < enemyInterval) {
-    return enemyDirection;
-  }
-  else {
-    lastEnemySet -= enemyInterval;
-  }
-  var left = 0;
-  if (enemyDirection.x < 0) {
-    left = BOX_NB_X + 2;
-  }
-  else if (enemyDirection.x > 0) {
-    left = - (ENEMY_MAX_SIZE + 1); 
-  }
-  var top = 0;
-  if (enemyDirection.y < 0) {
-    top = BOX_NB_Y + 2;
-  }
-  else if (enemyDirection.y > 0) {
-    top = - (ENEMY_MAX_SIZE + 1);
-  }
-  var box_nb_x = (left == 0) ? BOX_NB_X : ENEMY_MAX_SIZE;
-  var box_nb_y = (top == 0) ? BOX_NB_Y : ENEMY_MAX_SIZE;
-  enemies.push(getRandomVertice(left, top, box_nb_x, box_nb_y));
+  AbstractViewport.setEnemy(delta);
+  updateEnemies();
 }
 
-function moveEnemies(delta, direction) {
-  var toDelete = [];
-  var move = {
-    x: direction.x * 0.05*delta,
-    y: direction.y * 0.05*delta,
-  };
-  setGlobalMove(move);
-  var moveVertice = [
-    move.x, move.y, 0,
-    move.x, move.y, 0,
-    move.x, move.y, 0,
-    move.x, move.y, 0,
-  ];
-  for (var i=0; i<enemies.length; i++) {
-    enemies[i] = addMatrix(enemies[i], moveVertice);
-    if (outOfBounds(enemies[i], ENEMY_VIEWPORT_BOUNDS)) {
-      toDelete.unshift(i);
-    }
-  }
-  for (var i=0; i<toDelete.length; i++) {
-    enemies.splice(toDelete[i], 1);
+function emptyEnemies() {
+  AbstractViewport.emptyEnemies();
+  enemies = [];
+}
+
+function moveEnemies(delta) {
+  AbstractViewport.moveEnemies(delta, AbstractViewport.getRandomDirection(delta));
+  updateEnemies();
+}
+
+function updateEnemies() {
+  enemies = [];
+  var abstractEnemies = AbstractViewport.enemies;
+  for (var i=0; i<abstractEnemies.length; i++) {
+    enemies.push(multiplyMatrix(abstractEnemies[i], BOX_SIZE));
   }
 }
 
 // -------------------------------------------------------------------- VERTICES
+function multiplyMatrix(m1, size) {
+  var vertice = [];
+  for (var i=0; i<m1.length; i++) {
+    vertice.push(m1[i] * size);
+  }
+  return vertice;
+}
+
 function getRandomVertice(left, top, box_nb_x, box_nb_y) {
   var min = {
     x: 0,
@@ -168,10 +119,8 @@ function getRandomVertice(left, top, box_nb_x, box_nb_y) {
   randSize.y *= BOX_SIZE;
   randPosition.x += left;
   randPosition.x *= BOX_SIZE;
-  randPosition.x += enemyMove.x;
   randPosition.y += top;
   randPosition.y *= BOX_SIZE;
-  randPosition.y += enemyMove.y;
   var vertice = [
     randPosition.x, randPosition.y, 0.0,
     randPosition.x + randSize.x, randPosition.y, 0.0,
@@ -181,155 +130,10 @@ function getRandomVertice(left, top, box_nb_x, box_nb_y) {
   return vertice;
 }
 
-function addMatrix(m1, m2, bounds) {
-  if (m1.length != m2.length) {
-    return m1;
-  }
-  var clamp = {
-    x: 0.0,
-    y: 0.0,
-  };
-  if (bounds !== undefined) {
-    for (var i=0; i<m1.length && i<m2.length; i++) {
-      if (i%3 == 0) {
-        if (m1[i] + m2[i] < bounds.left && clamp.x <= 0) {
-          clamp.x -= m1[i] + m2[i] + bounds.left;
-        }
-        if (m1[i] + m2[i] > bounds.right && clamp.x >= 0) {
-          clamp.x -= m1[i] + m2[i] - bounds.right;
-        }
-      }
-      if (i%3 == 1) {
-        if (m1[i] + m2[i] < bounds.top && clamp.y <= 0) {
-          clamp.y -= m1[i] + m2[i] + bounds.top;
-        }
-        if (m1[i] + m2[i] > bounds.bottom && clamp.y >= 0) {
-          clamp.y -= m1[i] + m2[i] - bounds.bottom;
-        }
-      }
-    }
-  }
-  var vertice = [];
-  for (var i=0; i<m1.length && i<m2.length; i++) {
-    var newPos = m1[i] + m2[i];
-    if (i%3 == 0) {
-      newPos += clamp.x;
-    }
-    if (i%3 == 1) {
-      newPos += clamp.y;
-    }
-    vertice.push(newPos);
-  }
-  return vertice;
-}
-
-function outOfBounds(vertice4, bounds) {
-  for (var i=0; i<vertice4.length; i+=3) {
-    var vertex = vertice4.slice(i, i+2);
-    if (vertex[0] > bounds.left && vertex[0] < bounds.right &&
-        vertex[1] > bounds.top && vertex[1] < bounds.bottom) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function collision(v1, v2) {
-  var edges1 = getVerticeEdges(v1);
-  var edges2 = getVerticeEdges(v2);
-  if (edges1.top >= edges2.top && edges1.top <= edges2.bottom &&
-      edges1.left >= edges2.left && edges1.left <= edges2.right) {
-    return true;
-  }
-  if (edges1.top >= edges2.top && edges1.top <= edges2.bottom &&
-      edges1.right >= edges2.left && edges1.right <= edges2.right) {
-    return true;
-  }
-  if (edges1.bottom >= edges2.top && edges1.bottom <= edges2.bottom &&
-      edges1.left >= edges2.left && edges1.left <= edges2.right) {
-    return true;
-  }
-  if (edges1.bottom >= edges2.top && edges1.bottom <= edges2.bottom &&
-      edges1.right >= edges2.left && edges1.right <= edges2.right) {
-    return true;
-  }
-  return false;
-}
-
-function getVerticeEdges(vertice) {
-  var edges = { left:0, top:0, bottom:0, right:0 };
-  for (var i=0; i<vertice.length; i+=3) {
-    var vertex = vertice.slice(i, i+2);
-    if (i == 0) {
-      edges.left = vertex[0];
-      edges.right = vertex[0];
-      edges.top = vertex[1];
-      edges.bottom = vertex[1];
-    }
-    else {
-      if (vertex[0] < edges.left)
-        edges.left = vertex[0];
-      if (vertex[0] > edges.right)
-        edges.right = vertex[0];
-      if (vertex[1] < edges.top)
-        edges.top = vertex[1];
-      if (vertex[1] > edges.bottom)
-        edges.bottom = vertex[1];
-    }
-  }
-  return edges;
-}
-
-// ------------------------------------------------------------------------ MOVE
-var lastDirectionChange = 0;
-function getRandomDirection(delta) {
-  lastDirectionChange += delta;
-  if (enemyDirection != null && lastDirectionChange < 1000) {
-    return enemyDirection;
-  }
-  lastDirectionChange -= 1000;
-  if (enemyDirection == null || Math.floor(Math.random() * 10) == 0) {
-    var keys = Object.keys(DIRECTION);
-    var newDirection;
-    do {
-      newDirection = DIRECTION[keys[Math.floor(Math.random() * keys.length)]];
-    } while (newDirection == enemyDirection);
-    enemyDirection = newDirection;
-  }
-  return enemyDirection;
-}
-
-function setGlobalMove(move) {
-  addMoves(enemyMove, move, BOX_SIZE);
-}
-
-function addMoves(move1, move2, modulo, clamp) {
-  move1.x += move2.x;
-  move1.y += move2.y;
-  if (modulo !== undefined) {
-    move1.x %= modulo;
-    move1.y %= modulo;
-  }
-  if (clamp != undefined) {
-    if (move1.x > clamp.max) {
-      move1.x = clamp.max;
-    }
-    if (move1.y > clamp.max) {
-      move1.y = clamp.max;
-    }
-    if (move1.x < clamp.min) {
-      move1.x = clamp.min;
-    }
-    if (move1.y < clamp.min) {
-      move1.y = clamp.min;
-    }
-  }
-}
-
 // ------------------------------------------------------------------------ GAME
 var gameLoop;
 function startGame() {
-  enemies = [];
+  emptyEnemies();
   menu.style.display = "none";
   setPlayer();
   var delta = 0;
@@ -341,12 +145,10 @@ function startGame() {
       delta = newTime - oldTime;
       oldTime = newTime;
       setEnemy(delta);
-      moveEnemies(delta, getRandomDirection(delta));
+      moveEnemies(delta);
       movePlayer(delta);
-      for (var i=0; i<enemies.length; i++) {
-        if (collision(player,enemies[i])) {
-          stopGame();
-        }
+      if (AbstractViewport.playerCollisionWithEnemies()) {
+        stopGame();
       }
     },
     16
@@ -355,8 +157,8 @@ function startGame() {
 
 function stopGame() {
   clearInterval(gameLoop);
-  enemies = [];
-  player = [];
+  emptyEnemies();
+  emptyPlayer();
   showMenu();
 }
 
@@ -380,22 +182,7 @@ window.addEventListener("keydown", function (event) {
   if (event.defaultPrevented) {
     return;
   }
-  switch (event.keyCode) {
-    case 40:
-      addMoves(playerDirection, DIRECTION.bottom, undefined, { max:1, min:-1 });
-      break;
-    case 38:
-      addMoves(playerDirection, DIRECTION.top, undefined, { max:1, min:-1 });
-      break;
-    case 37:
-      addMoves(playerDirection, DIRECTION.left, undefined, { max:1, min:-1 });
-      break;
-    case 39:
-      addMoves(playerDirection, DIRECTION.right, undefined, { max:1, min:-1 });
-      break;
-    default:
-      return;
-  }
+  AbstractViewport.playerEvent(event);
   event.preventDefault();
 }, true);
 
@@ -403,22 +190,7 @@ window.addEventListener("keyup", function (event) {
   if (event.defaultPrevented) {
     return;
   }
-  switch (event.keyCode) {
-    case 40:
-      addMoves(playerDirection, DIRECTION.top, undefined, { max:1, min:-1 });
-      break;
-    case 38:
-      addMoves(playerDirection, DIRECTION.bottom, undefined, { max:1, min:-1 });
-      break;
-    case 37:
-      addMoves(playerDirection, DIRECTION.right, undefined, { max:1, min:-1 });
-      break;
-    case 39:
-      addMoves(playerDirection, DIRECTION.left, undefined, { max:1, min:-1 });
-      break;
-    default:
-      return;
-  }
+  AbstractViewport.playerEvent(event);
   event.preventDefault();
 }, true);
 
